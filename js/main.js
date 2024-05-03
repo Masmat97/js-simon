228e3b9a28805b4460567f3c06ea73c7e63e459c
Matteo Mascio <matteo.mascio@libero.it>
2024-05-03 15:36:59 +0200

    M1

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// function getRandomInt(max) {
+//     return Math.floor(Math.random() * max);
+//   }
+//   console.log(getRandomInt(10));
+//   console.log(getRandomInt(10));
+//   console.log(getRandomInt(10));
+//   console.log(getRandomInt(10));
+//   console.log(getRandomInt(10));
+
+  function casuali() {
+    n = parseInt(document.getElementById("quanti").value);
+    if (n > 0 && n <= 100) {
+      str = "";
+      for (i = 0; i < n; i++) {
+           num = Math.round(Math.random()*100 + 1);
+           if (i > 0) {
+              str += ", ";
+           }
+           str += num;
+       }
+     document.getElementById("casuali").innerHTML = "Numeri generati: " + str;
+     } 
+     else {
+        document.getElementById("casuali").innerHTML = "Inserisci un numero maggiore di 0 e inferiore a 100";
+     }
+  }
